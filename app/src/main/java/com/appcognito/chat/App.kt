b6285a31cfc4e19041eddb21be98da46bfe20d93
Will-Kim <ipapaya2@gmail.com>
2024-05-30 09:26:56 +0900

    initial hilt base app

---
+++ app/src/main/java/com/appcognito/chat/App.kt
@@ -0,0 +1,11 @@
+package com.appcognito.chat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
